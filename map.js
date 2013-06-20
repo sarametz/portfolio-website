a378f5ff59d0a6c9a8f3c78abf23efb447541e9f
Sara Metz <smetz@hotmail.co.nz>
2013-06-21 11:06:50 +1200

    Initial commit with earliest copy found

---
+++ map.js
@@ -0,0 +1,22 @@
+var map;
+
+function initialise(){
+	var myOptions = {
+						zoom:5,
+						center:new google.maps.LatLng(-39.724089,173.778305),
+						mapTypeId: google.maps.MapTypeId.ROADMAP
+						};
+
+	map = new google.maps.Map(document.getElementById('map_canvas'),myOptions);
+	markMap();
+	window.console.log("map made");
+}
+
+function markMap(){
+	var options = {	
+					position: new google.maps.LatLng(-36.844461,174.745102)
+				  	};
+	var marker = new google.maps.Marker(options);
+	marker.setMap(map);
+	
+}
