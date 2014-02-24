1c6aff0701ad7854b4f63cce3ff808c91ab658e5
Sara Metz <smetz@hotmail.co.nz>
2014-02-24 14:33:41 +1300

    Updated scrolling

--- script.js
+++ script.js
@@ -4,25 +4,11 @@
  */
 
 $(window).load(function() {
-	/////////////
-	//Make start on content 
-	//TESTING ONLY
-
-	// move navbar to left
-			$("#static-nav").animate({"left": $("#sidebar").outerWidth()-$("#static-nav").outerWidth() }, 10, 
-								function(){ //set sidebar to fixed once finished
-									$("#sidebar").css({ "position": "fixed", "right": "auto", "left":"0%"});
-									$("#home").addClass("hide");
-								});
-			$("#content-wrapper").animate({"left": 0}, 10);
-
-	////////////
 
 	$("#portfolio, #about, #contact").css("height", $(window).height() );
 	$("#nav-container").css("margin-top", - $("#nav-container").height()/2 );
 	$("#project-grid").css("margin-top", - $("#project-grid").height()/2 );
 	$("#about-content").css("margin-top", - $("#about-content").height()/2 );
-	console.log($("#project-carousel").height());
 	$("#project-carousel").css("margin-top", - $("#project-carousel").height()/2 );
 	$("#project-carousel").addClass("hidden");
 
@@ -57,7 +43,7 @@ $(window).load(function() {
 			$("#static-nav").animate({"left": $("#sidebar").outerWidth()-$("#static-nav").outerWidth() }, 750, 
 								function(){ //set sidebar to fixed once finished
 									$("#sidebar").css({ "position": "fixed", "right": "auto", "left":"0%"});
-									$("#home").addClass("hide");
+									$("#home").addClass("hidden");
 								});
 			$("#content-wrapper").animate({"left": 0}, 750);			
 			
@@ -70,12 +56,69 @@ $(window).load(function() {
 		$("#sidebar li").removeClass("active");
 		$(this).parent().addClass("active");
 
-		$("#home").removeClass("hide");
+		$("#home").removeClass("hidden");
 
 		$("#static-nav").animate({"left": "0%" },750);
 		$("#content-wrapper").animate({"left": "100%"},750);
 	});
 	
+	$("#content").on("mousewheel wheel", function(event){
+		event.preventDefault();
+		//don't do anything if animating already
+		if($(":animated").length > 0){
+			return false;
+		}
+
+		if(event.originalEvent.wheelDelta > 0 || event.originalEvent.deltaY < 0) {
+	         //scroll up
+	        console.log('mouseW Up');
+	        var offSet = 50;
+
+			var diffs = [ 	offSet - $("#portfolio").offset().top,
+         					offSet - $("#about").offset().top,
+         					offSet - $("#contact").offset().top	];
+
+         	// remove negative from array 
+         	diffs = diffs.filter(function(element){
+         		return element > 0;
+         	});
+
+         	//get smallest element
+         	var min = Math.min.apply(Math,diffs);
+         	$('#content').animate({scrollTop:($("#content").scrollTop() - min)}, { duration:750, queue:false, complete: function(){
+         			updateActiveNav();
+         		} 
+         	});
+
+	     }else {
+	        //scroll down
+	        console.log('mouseW Down');
+	        var offSet = 50;
+
+			var diffs = [ 	offSet - $("#portfolio").offset().top,
+         					offSet - $("#about").offset().top,
+         					offSet - $("#contact").offset().top	];
+
+         	// remove positive and small negative from array 
+         	diffs = diffs.filter(function(element){
+         		return element < -1;
+         	});
+
+         	if(diffs.length > 0){
+	         	//get largest element
+	         	var max = Math.max.apply(Math,diffs);
+	         	$('#content').animate({scrollTop:($("#content").scrollTop() - max)}, { duration:750, queue:false, complete: function(){
+         				updateActiveNav();
+         			} 
+         		});
+         	}
+	     }
+
+	     
+	     //prevent page fom scrolling
+	     return false;
+	});
+
 	$(".project-cell").on("click", function(event){
 		//move carousel to selected slide
 		//show carousel and hide thumbs
@@ -88,6 +131,47 @@ $(window).load(function() {
 		$("#project-grid").removeClass("hidden");
 		$("#project-carousel").addClass("hidden"); 
 	});
+
+	function updateActiveNav(){
+		//don't do anything if animating
+		if($(":animated").length > 0){
+			return false;
+		}
+		$("#sidebar li").removeClass("active");
+		
+		// if home showing 
+		if(!$("#home").hasClass("hidden")){
+			$("a[data-id=\"home\"]").parent().addClass("active");		
+		}else{
+			var threshold = 100;
+			//get tops
+			var pTop = $("#portfolio").offset().top;
+			var aTop = $("#about").offset().top;
+			var cTop = $("#contact").offset().top;
+
+			console.log(" top = " + $("#content").scrollTop());
+			console.log("ptop = " + pTop+" a="+aTop+" c="+cTop);
+
+			if ( pTop < threshold && pTop > -threshold) {
+	            $("a[data-id=\"portfolio\"]").parent().addClass("active");
+
+        	}else if( aTop < threshold && aTop > -threshold) {
+	            $("a[data-id=\"about\"]").parent().addClass("active");
+
+        	}else if(cTop < threshold && cTop > -threshold){
+	            $("a[data-id=\"contact\"]").parent().addClass("active");
+        	}
+
+		}
+		
+	}
+
+		/////////////
+	//Make start on content 
+	//TESTING ONLY
+	$("a[data-id=\"portfolio\"]").click();
+	updateActiveNav();
+	////////////
 
 });
 
